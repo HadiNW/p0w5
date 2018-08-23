2aaeaa53e2cf8cd5887403ac4bc2b9bd77e89d49
Hadi NW <hadinw@Hadis-MacBook-Pro.local>
2018-08-23 22:34:10 +0700

    update task 7

--- task7.hapusSimbol.js
+++ task7.hapusSimbol.js
@@ -1,28 +1,22 @@
 function hapusSimbol(str) {
-    var character
-  var text = ''
-  var kamus = 'abcdefghijklmnopqrstuvwxyz0987654321';
-  for (var i = 0; i < kamus.length; i++) {
-      for (j = 0; j < str.length; j++) {
-        
-          if (str[j] === kamus[i]) {
-             character = str[i]
-          } else {
-            character = ''
-           
-          }
-          text += character
-          console.log(text)
-      }
-  }
-  //return text
-    }
-   
-
-
-// TEST CASES
+    var chars = '';
+    var text = '';
+    var kamus = 'abcdefghijklmnopqrstuvwxyz0987654321';
+   for (var i = 0; i < str.length; i++) {
+       for (var j = 0; j < kamus.length; j++) {
+           chars = str[i]
+           if (str[i] !== kamus[j]) {
+               chars = ''        
+           }
+           text += chars;
+       }
+       
+   }
+    return text;
+}
+  // TEST CASES
 console.log(hapusSimbol('test%$4aa')); // test4aa
-// console.log(hapusSimbol('devel0p3r s3j@@ati')); // devel0p3rs3jati
-// console.log(hapusSimbol('ma@#k!an~')); // makan
-// console.log(hapusSimbol('coding')); // coding
-// console.log(hapusSimbol('1+3-5*2=100')); // 1352100
+console.log(hapusSimbol('devel0p3r s3j@@ati')); // devel0p3rs3jati
+console.log(hapusSimbol('ma@#k!an~')); // makan
+console.log(hapusSimbol('coding')); // coding
+console.log(hapusSimbol('1+3-5*2=100')); // 1352100
