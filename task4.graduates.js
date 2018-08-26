47187755c832417c0af275a58e3ccc706f944ab3
Hadi NW <hadinw@Hadis-MacBook-Pro.local>
2018-08-26 19:55:31 +0700

    update

---
+++ task4.graduates.js
@@ -0,0 +1,108 @@
+function graduates (students) {
+  var result = {};  
+
+  for (var i = 0; i < students.length; i++) {
+      var studentClass = students[i].class;
+      var studentScore = students[i].score;   
+      var studentName = students[i].name;  
+      
+      if (result[studentClass] === undefined) {
+          result[studentClass] = [];
+          if (studentScore > 75) {
+           
+              result[studentClass].push({
+                  name: studentName,
+                  score: studentScore
+              });
+          }
+      } else {        
+        if (studentScore > 75) {
+            result[studentClass].push({
+                name: studentName,
+                score: studentScore
+            });
+        }
+      }
+     
+  }
+  return result
+}
+ 
+
+
+console.log(graduates([
+  {
+    name: 'Dimitri',
+    score: 90,
+    class: 'foxes'
+  },
+  {
+    name: 'Alexei',
+    score: 85,
+    class: 'wolves'
+  },
+  {
+    name: 'Sergei',
+    score: 74,
+    class: 'foxes'
+  },
+  {
+    name: 'Anastasia',
+    score: 78,
+    class: 'wolves'
+  }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Dimitri', score: 90 }
+//   ],
+//   wolves: [
+//     { name: 'Alexei' , score: 85 },
+//     { name: 'Anastasia', score: 78 }
+//   ]
+// }
+
+console.log(graduates([
+  {
+    name: 'Alexander',
+    score: 100,
+    class: 'foxes'
+  },
+  {
+    name: 'Alisa',
+    score: 76,
+    class: 'wolves'
+  },
+  {
+    name: 'Vladimir',
+    score: 92,
+    class: 'foxes'
+  },
+  {
+    name: 'Albert',
+    score: 71,
+    class: 'wolves'
+  },
+  {
+    name: 'Viktor',
+    score: 80,
+    class: 'tigers'
+  }
+]));
+
+// {
+//   foxes: [
+//     { name: 'Alexander', score: 100 },
+//     { name: 'Vladimir', score: 92 }
+//   ],
+//   wolves: [
+//     { name: 'Alisa', score: 76 },
+//   ],
+//   tigers: [
+//     { name: 'Viktor', score: 80 }
+//   ]
+// }
+
+
+console.log(graduates([])); //{}
